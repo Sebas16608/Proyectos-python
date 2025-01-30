d500606bbeb453dddb616bac4cdb445fbd6871af
Sebas <Asebasrr444@gmail.com>
2025-01-29 22:28:32 -0600

    nuevos cambios y cosas
    cosas nuevas

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # Proyectos-python
- proyectos para practicar
-Este repocitorio contendra varios proyectos en los cuales van a servirme a mi como developer aprender y practicar
-# python_1
-es el primer proyecto de encriptacion de texto
+Proyectos de aprendizaje
+# primer proyecto
+tipos de variables
--- python_1/proyecto1.py
+++ python_1/proyecto1.py
@@ -1,1 +1,19 @@
-#primer proyecto
+#primer proyecto tipos de variables
+#tipo cadena str
+hola="hola mundo"
+print(hola)
+print(type(hola))
+#tipo entero "int"
+uno=1
+print(1)
+print(type(1))
+#tipo flotante "float"
+ret=1.5
+print(ret)
+print(type(ret))
+#concatenar
+name="Sebastian"
+edad=16
+estatura=1.80
+#imprimir concatenacion
+print("Hola"+ name + "tu edad es: " + edad + "y mides: " + estatura)
