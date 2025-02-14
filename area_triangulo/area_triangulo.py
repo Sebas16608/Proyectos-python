3c03d28ef7956c69c81f95643a963adbf6860493
Sebas <Asebasrr444@gmail.com>
2025-02-13 18:47:58 -0600

    ejercicio de variables
    calcular el area de un triangulo

---
+++ area_triangulo/area_triangulo.py
@@ -0,0 +1,12 @@
+print("Hola, hoy vamos a carlcular el area de un triangulo \n")
+print("Por favor ingrese los datos necesarios \n")
+
+#pedimos la altura
+altura = float(input("Por favor ingrese la altura: "))
+
+#pedimos la base
+base = float(input("Por favor ingrese la base: "))
+
+#calculamos e imprimimos el area
+area = base*altura/2
+print(f"El area de su triangulo es: {area}")
