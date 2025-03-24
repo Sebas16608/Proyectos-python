1a3a752885946ee28bb2b5c672c26f2493daada3
Sebas <Asebasrr444@gmail.com>
2025-03-23 20:40:52 -0600

    Sacar el promedio de 5 clases de una lista

---
+++ Ejercicios_Listas_Y_Diccionarios/Promedio.py
@@ -0,0 +1,12 @@
+clases = [8, 9, 4, 5, 7] #calificaciones guardadas
+
+#variable para sumar todas las materias
+suma = sum(clases)
+
+# variable que sirve para poner la cantidad de elementos
+cantidad = len(clases)
+
+# y aca se saca el promedio
+promedio = suma / cantidad
+
+print(f"El promedio de las clases es: {promedio}")
