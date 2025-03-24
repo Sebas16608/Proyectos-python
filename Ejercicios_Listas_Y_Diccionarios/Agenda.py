42bf4f1560448cbcac529847897276bdefc80217
Sebas <Asebasrr444@gmail.com>
2025-03-24 15:32:02 -0600

    Avance hasta el punto 3 que sirve para modificar un numero en la agenda

--- Ejercicios_Listas_Y_Diccionarios/Agenda.py
+++ Ejercicios_Listas_Y_Diccionarios/Agenda.py
@@ -12,8 +12,40 @@ contador = True
 while True:
     print("Agenda Telefónica \n")
     print("--------------------------")
-    print("1. Consultar \n 2. añadir \n 3. Modificar \n 4. Borrar \n 5. Salir")
+    print("1. Consultar \n2. añadir \n3. Modificar \n4. Borrar \n5. Salir")
 
-    opcion = input("-> ")
+    opcion = int(input("-> "))
 
+    while opcion not in ("1", "2", "3", "4", "5"):
+        print("Opcion no valida elija la opcion correcta")
+        opcion = int(input("->"))
 
+    # es la parte de consultas osea la opcion 1
+    if opcion == 1:
+        # pedir nombre
+        name = input("Ingrese el nombre: ")
+        # verificar si el nombre esta en el diccionario
+        if name not in agenda:
+            print("Nombre no disponible")
+        else:
+            telefono = agenda[name]
+            print(agenda[name], agenda[telefono])
+    # es la parte para añadir osea la opcion 2
+    elif opcion == 2:
+        name = input("Nombre: ")
+        if name in agenda:
+            print("Nombre ya registrado")
+        else:
+            telefono = int(input("Numero: "))
+            # añadir el telefono agregado recientemente
+            agenda[name] = telefono
+            print("Registrado exitosamente.")
+    elif opcion == 3:
+        name = input("Nombre: ")
+        if name not in agenda:
+            print("El nombre no esta en la agenda para poder modificarlo.")
+        else:
+            telefono = int(input("Numero: "))
+            # Añadir el nuevo numero a la agenda
+            agenda[name] = telefono
+            print("Numero modificado exitosamente.")
