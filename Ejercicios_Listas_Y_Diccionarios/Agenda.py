278d183c8f6f290557608414242b7f9d38d38481
Sebas <Asebasrr444@gmail.com>
2025-03-23 23:15:47 -0600

    En proceso de terminar la agenda y mi noviecita me ama mucho

--- Ejercicios_Listas_Y_Diccionarios/Agenda.py
+++ Ejercicios_Listas_Y_Diccionarios/Agenda.py
@@ -1,3 +1,4 @@
+# Diccionario con base de datos
 agenda = {
     "Mario": 55689078,
     "Juan": 30085258,
@@ -7,9 +8,12 @@ agenda = {
 
 contador = True
 
+# Usando un bucle while para poder pedirle al usuario cuantas veces quiera hacer lo que necesite
 while True:
     print("Agenda Telefónica \n")
     print("--------------------------")
     print("1. Consultar \n 2. añadir \n 3. Modificar \n 4. Borrar \n 5. Salir")
+
+    opcion = input("-> ")
 
 
