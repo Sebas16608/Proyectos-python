feae4fe1b700559723b994c63dac41653d726877
Sebas <Asebasrr444@gmail.com>
2025-03-23 22:08:42 -0600

    Empiezo la agenda

---
+++ Ejercicios_Listas_Y_Diccionarios/Agenda.py
@@ -0,0 +1,15 @@
+agenda = {
+    "Mario": 55689078,
+    "Juan": 30085258,
+    "Pedro": 5548975,
+    "Maria": 53647890 
+}
+
+contador = True
+
+while True:
+    print("Agenda Telefónica \n")
+    print("--------------------------")
+    print("1. Consultar \n 2. añadir \n 3. Modificar \n 4. Borrar \n 5. Salir")
+
+
