d4b8552bc895749ac4bf3dad4fc80b9de8bca090
Sebas <Asebasrr444@gmail.com>
2025-03-24 18:29:56 -0600

    Termine el programa y arregle unos errores que tenia en los condicionales, pero ya esta

--- Ejercicios_Listas_Y_Diccionarios/Agenda.py
+++ Ejercicios_Listas_Y_Diccionarios/Agenda.py
@@ -6,22 +6,21 @@ agenda = {
     "Maria": 53647890 
 }
 
-contador = True
+consultar = True
 
 # Usando un bucle while para poder pedirle al usuario cuantas veces quiera hacer lo que necesite
-while True:
+while consultar:
     print("Agenda Telefónica \n")
     print("--------------------------")
     print("1. Consultar \n2. añadir \n3. Modificar \n4. Borrar \n5. Salir")
 
-    opcion = int(input("-> "))
+    opcion = input("-> ")
 
     while opcion not in ("1", "2", "3", "4", "5"):
-        print("Opcion no valida elija la opcion correcta")
-        opcion = int(input("->"))
-
+        print("Opción no válida, por favor elige una opción correcta.")
+        opcion = input("-> ")
     # es la parte de consultas osea la opcion 1
-    if opcion == 1:
+    if opcion == "1":
         # pedir nombre
         name = input("Ingrese el nombre: ")
         # verificar si el nombre esta en el diccionario
@@ -31,7 +30,7 @@ while True:
             telefono = agenda[name]
             print(agenda[name], agenda[telefono])
     # es la parte para añadir osea la opcion 2
-    elif opcion == 2:
+    elif opcion == "2":
         name = input("Nombre: ")
         if name in agenda:
             print("Nombre ya registrado")
@@ -40,7 +39,7 @@ while True:
             # añadir el telefono agregado recientemente
             agenda[name] = telefono
             print("Registrado exitosamente.")
-    elif opcion == 3:
+    elif opcion == "3":
         name = input("Nombre: ")
         if name not in agenda:
             print("El nombre no esta en la agenda para poder modificarlo.")
@@ -49,3 +48,14 @@ while True:
             # Añadir el nuevo numero a la agenda
             agenda[name] = telefono
             print("Numero modificado exitosamente.")
+    elif opcion == "4":
+        name = input("Nombre: ")
+        if name not in agenda:
+            print("El nombre no esta en la agenda")
+        else:
+            del agenda[name]
+            print("El Contacto se ha eliminado correctamente")
+
+    elif opcion == "5":
+        consultar = False
+        print("Gracias por utilizar la agenda :D hasta pronto...")
